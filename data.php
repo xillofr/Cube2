5273e37b237e6810a4776947a16a7c46d07a1a04
SerialFrostiz <jeremy.mikolajczak@laposte.net>
2022-01-29 00:51:14 +0100

    ajout premier jet page chart + edit chart.html + edit data php pour requete differente + ajout idées/améliorations

--- data.php
+++ data.php
@@ -1,9 +1,19 @@
 
 
 <?php 
+//on défini la fonction qui récupère les datas et les transforme en json
 function getData(){
+
+// on crée le tableau qui contiendra nos valeurs    
 $display = array();
 
+//on récupère le paramètre envoyé par le client
+$lastnombre = htmlspecialchars($_GET["lastnombre"]);
+
+//echo $valeurtest;
+//$valeurtest = valeurtest;
+
+//on initialise la connection avec la DB
 try {
     $dbh = new PDO('mysql:host=192.168.1.26;dbname=cube2', 'root', 'root');
 } catch (PDOException $e) {
@@ -11,21 +21,42 @@ try {
     die();
 }
 
-$stmt = $dbh->prepare("SELECT * from entries order by id_entry DESC limit 15");
+//on défini la procédure selon l'affichage désiré et on stock dans $stmt :
 
-// appel de la procédure stockée
+//ici pour toutes les entrées
+if($lastnombre == "all"){
+    $stmt = $dbh->prepare("SELECT * from entries order by id_entry DESC");
+}
+
+//ici pour le mois (31jours * 24 entrées = 744)
+if($lastnombre == "month"){
+    $stmt = $dbh->prepare("SELECT * from entries order by id_entry DESC limit 744");
+}
+
+//ici par semaine (7j * 24entrées = 168)
+if($lastnombre == "week"){
+    $stmt = $dbh->prepare("SELECT * from entries order by id_entry DESC limit 168");
+}
+
+//ici pour les affichages personalisés
+if(is_numeric($lastnombre)){
+    $stmt = $dbh->prepare("SELECT * from entries order by id_entry DESC limit $lastnombre");
+}
+
+//on execute la procédure stockée
 $stmt->execute();
 while($data = $stmt->fetch(PDO::FETCH_ASSOC)){
-    //print $data['id_entry'] . $data['temperature'] . $data['humidite'];
-    //print_r($data);
-array_push($display,$data);  
+    // on ajoute le résultat de la procédure dans un tableau pour chaque ligne du résultat de la requête
+    array_push($display,$data);  
 }
-//print "La procédure a retourné : $value\n";
-   // print_r($display);
+
+//on encode le tableau vers un json
 echo json_encode($display, JSON_NUMERIC_CHECK);
 return;
 }
 
+//on affiche le json pour le graphique
 echo getData();
+
 ?>
 
